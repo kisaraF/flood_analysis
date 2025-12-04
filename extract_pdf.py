0d278c200cbfb2a74652235934ecd59281c02516
kisaraF <kisarawenu@gmail.com>
2025-12-04 21:15:48 +0530

    Update with Latest Data & Fix PDF extraction for docs with multiple pages and tables

--- extract_pdf.py
+++ extract_pdf.py
@@ -14,14 +14,18 @@ def get_latest_pdf(pdf_ls: list) -> str:
 
 def pdf_to_json(file: str, epoch: str) -> None:
     status_mg = None
-    table_ls = camelot.read_pdf(file)
+    table_ls = camelot.read_pdf(file, pages='all')
+    tables_length = [i.df.shape[0] for i in table_ls]
+    tables_width = [i.df.shape[1] for i in table_ls]
     if len(table_ls) == 1:
         table_ls[0].to_json(f"{os.getcwd()}/Data/JSON/water_level_{epoch}.json")
         status_mg = (
             f'PDF table read and written to a JSON as "water_level_{epoch}.json"'
         )
     else:
-        status_mg = "Error! More than 1 table identified"
+        table_selected_ls = [i for i in table_ls if i.df.shape == (max(tables_length), max(tables_width))]
+        table_selected_ls[0].to_json(f"{os.getcwd()}/Data/JSON/water_level_{epoch}.json")
+        status_mg = "More than 1 table identified. Selected only the one with longest length and widest body"
 
     lg.logging.info(status_mg)
 
@@ -31,7 +35,7 @@ def main():
     lg.init_logger(log_type)
     pdf_ls = get_pdf_list()
     latest_report_epoch = get_latest_pdf(pdf_ls)
-    pdf_item = [i for i in get_pdf_list() if latest_report_epoch in i][0]
+    pdf_item = [i for i in pdf_ls if latest_report_epoch in i][0]
     pdf_url = f"{os.getcwd()}/Data/PDF/{pdf_item}"
     pdf_to_json(pdf_url, latest_report_epoch)
 
--- parse_json.py
+++ parse_json.py
@@ -2,6 +2,7 @@ import os
 import json
 from Logger import init_logger as lg
 from datetime import datetime
+from zoneinfo import ZoneInfo
 import duckdb
 import polars as pl
 import re
@@ -64,7 +65,7 @@ def process_dataframe(
         f"{os.getcwd()}/maps/river_basinc.json", "r"
     ) as rbj:  # River basins are not properly filled out. Mapping and filling them
         river_basin_map = json.load(rbj)
-    epoch_h = datetime.fromtimestamp(int(epoch)).strftime(
+    epoch_h = datetime.fromtimestamp(int(epoch), tz=ZoneInfo("Asia/Colombo")).strftime(
         "%Y%m%d%H%M%S"
     )  # Adding a human readable timestamp for the report epoch
     df = pl.DataFrame(content_rows, schema=header_cols)
@@ -130,7 +131,7 @@ def process_dataframe(
 
     for i in dynamic_cols:
         df = df.with_columns(
-            pl.col(i).str.replace_all(r"NA|-|N.A.", "0").cast(pl.Float64)
+            pl.col(i).str.replace_all(r"NA|-|N.A.|^$", "0").cast(pl.Float64)
         )  # Replace dynamic columns NA values as 0 for convenient type conversions
 
     df = df.with_columns(  # Derived column with analytics value
